d17e2883d249878b5ebe9cac84edbafa1a7443e2
namestesensei <christopherwright435@gmail.com>
2024-08-16 04:44:24 +0000

    Fix formatting and documentation issues in sort.h to comply with Betty style
    - Removed unnecessary spaces at the beginning of lines in sort.h to comply with Betty style guidelines.
    - Ensured all function prototypes are properly documented following Betty's documentation style.
    - Verified that the struct definition and function prototypes are correctly formatted with no trailing spaces or misaligned indentations.
    - Re-ran Betty style checker to confirm that the file meets all required coding standards without warnings or errors.

--- sort.h
+++ sort.h
@@ -3,29 +3,61 @@
 
 #include <stddef.h>
 
-/* Definition of the doubly linked list structure */
+/**
+ * struct listint_s - Doubly linked list node structure
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous node in the list
+ * @next: Pointer to the next node in the list
+ */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
-/* Function to preformv bubble sort */
-void bubble_sort(int *array, size_t size0);
 
-/* Function to print an array of integers */
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using
+ * the Bubble sort algorithm.
+ * @array: Pointer to the array of integers
+ * @size: Number of elements in the array
+ */
+void bubble_sort(int *array, size_t size);
+
+/**
+ * print_array - Prints an array of integers
+ * @array: Pointer to the array of integers
+ * @size: Number of elements in the array
+ */
 void print_array(const int *array, size_t size);
 
-/* Function to print a doubly linked list */
+/**
+ * print_list - Prints a doubly linked list
+ * @list: Pointer to the head of the list
+ */
 void print_list(const listint_t *list);
 
-/* Function to perform insertion sort on a doubly linked list */
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in
+ * ascending order using the Insertion sort algorithm.
+ * @list: Pointer to the head of the list
+ */
 void insertion_sort_list(listint_t **list);
 
-/* Function to perform selection sort on an array of integers */
+/**
+ * selection_sort - Sorts an array of integers in ascending order using
+ * the Selection sort algorithm.
+ * @array: Pointer to the array of integers
+ * @size: Number of elements in the array
+ */
 void selection_sort(int *array, size_t size);
 
-/* Function to perform quick sort on an array of integers */
+/**
+ * quick_sort - Sorts an array of integers in ascending order using
+ * the Quick sort algorithm.
+ * @array: Pointer to the array of integers
+ * @size: Number of elements in the array
+ */
 void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
